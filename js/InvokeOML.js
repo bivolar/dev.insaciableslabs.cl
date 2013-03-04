bdd660c8b0ba5765d065a88a5e3d2c3737110f37
bivolar <carlobaeza@gmail.com>
2013-03-03 22:07:48 -0300

    testiando primer commit

---
+++ js/InvokeOML.js
@@ -0,0 +1,6 @@
+var __vuconnector_connector_id = 'covu';
+{
+  var __oml_static_script = document.createElement('script');
+  __oml_static_script.src = 'http://3.1cld.com/?doc=http://oml.optini.s3.amazonaws.com/covu.xml';
+  document.body.appendChild(__oml_static_script);
+};
